4084b7b9238cd48c340811f1e18e1ab3f2b2b0f5
nadiaemg <nademarg@gmail.com>
2024-06-03 16:20:07 -0600

    Se agregó funcionalidad a los botones e imagen de fondo al encabezado en tamaño responsivo.

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-const semblanza = document.getElementById('semblanzaTitulo');
-const btnMostrarOcultar = document.getElementById('btnMostrarOcultar');
-const leerContenido = document.getElementById('leerContenido');
 const year = document.getElementById('year');
-const forms = document.querySelectorAll('.needs-validation')
+const semblanza = document.getElementById('semblanzaTitulo');
+const leerContenido = document.getElementById('leerContenido');
+const btnMostrarOcultar = document.getElementById('btnMostrarOcultar');
+const btnContacto = document.getElementById('btnContacto');
+const btnSemblanza = document.getElementById('btnSemblanza');
+const forms = document.querySelectorAll('.needs-validation');
 const nombre = document.getElementById('nombre');
 const correo = document.getElementById('correo');
 const telefono = document.getElementById('telefono');
 const mensaje = document.getElementById('mensaje');
 const politicas = document.getElementById('politicas');
-const secureToken = "";
-const correoDestinatario = "nademarg@gmail.com"
+const secureToken = "b68fdc73-bb4f-4596-9cee-3567be94d4b4";
+const correoDestinatario = "naluracusbo@gmail.com"
+const celularDestinatario  = "tel:+525525584098"
+const mensajeWhatsapp = "https://api.whatsapp.com/send?phone=5215525584098&text=Hola%20Ivonne%2C%20%C2%BFme%20podr%C3%ADas%20proporcionar%20m%C3%A1s%20informaci%C3%B3n%20para%20agendar%20una%20sesi%C3%B3n%20de%20psicoterapia%3F"
 
 /*Enviar correo de contacto*/
 function enviarMensaje(){
@@ -71,6 +75,15 @@ Array.from(forms).forEach(form => {
   }, false);
 });
 
+let enviarWhatsapp = () => window.location.href = mensajeWhatsapp;
+let llamar = () => window.open(celularDestinatario);
+
+window.addEventListener('load', () => {year.innerText = new Date().getFullYear();
+  
+});
+btnContacto.addEventListener("click", () => {window.location.href = './index.html#contacto'});
+btnSemblanza.addEventListener("click", () => {window.location.href = './index.html#semblanza'});
+
 /*Botón "Leer más/menos*/
 btnMostrarOcultar.addEventListener('click', function () {
   console.log("click");
@@ -83,10 +96,4 @@ btnMostrarOcultar.addEventListener('click', function () {
     btnMostrarOcultar.innerText = "Leer más..."
     semblanza.focus();
   }
-});
-
-window.addEventListener('load', function(event){
-  event.preventDefault();
-  year.innerText = new Date().getFullYear();
-
 });
